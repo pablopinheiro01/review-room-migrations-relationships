b99aaa3ccf985fea4a785e7f4a130f7c254821a7
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2022-09-29 21:01:59 -0300

    Implementado uma high order function para alteração da intent

--- app/src/main/java/br/com/alura/orgs/extensions/ContextExtensions.kt
+++ app/src/main/java/br/com/alura/orgs/extensions/ContextExtensions.kt
@@ -3,9 +3,13 @@ package br.com.alura.orgs.extensions
 import android.content.Context
 import android.content.Intent
 
-fun Context.vaiPara(clazz: Class<*>) {
+fun Context.vaiPara(
+    clazz: Class<*>,
+    intent: Intent.() -> Unit = {} // pode receber ou nao uma intent via construtor modificada
+) {
     Intent(this, clazz)
         .apply {
+            intent() //modifica a intent recebida
             startActivity(this)
         }
 }
